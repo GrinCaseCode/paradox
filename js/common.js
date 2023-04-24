ffd926b4c52d0d7415125918e63357a59a035370
nickolay.grin <nick@singularitysolutions.com>
2023-04-24 17:04:39 +0300

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -19,7 +19,9 @@ if ( $(this).scrollTop() > 0 && $menu.hasClass("default") ){
   }
 
 	//плавный скролл
-	$(".navigat li a").mPageScroll2id();
+	$(".nav-article li a").mPageScroll2id({
+		offset: 70
+	});
 
 
 
